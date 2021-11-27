b0903195379dc4288b23eeca15cf39ebda45ee13
NickPYTH <nicksrnk@gmail.com>
2021-11-27 23:13:47 +0500

    changed responce

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,7 @@
 from django.urls import include, path
-from .views import CreateProfile, GetProfile, UpdateProfile
+from .views import CreateProfile, GetProfile
 
 urlpatterns = [
     path("create-profile", CreateProfile.as_view(), name="create-profile"),
     path("get-profile", GetProfile.as_view(), name="get-profile"),
-    path("update-profile", UpdateProfile.as_view(), name="update-profile"),
 ]
