b4e65dd7f17bc3d625abdf008d20b281d9b0c639
NickPYTH <nicksrnk@gmail.com>
2021-11-27 18:16:50 +0500

    changed responce

--- accounts/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import userProfile
-
-admin.site.register(userProfile)
-
-admin.site.site_header = "voteAppAdmin"
--- accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-
-class userProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-    
