67b66ec0880e765387ffb489792a1dd616a70d86
NickPYTH <nicksrnk@gmail.com>
2021-11-27 17:40:05 +0500

    init

---
+++ accounts/serializers.py
@@ -0,0 +1,37 @@
+from validate_email import validate_email
+from rest_framework import serializers
+from .models import userProfile
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import check_password, make_password
+
+
+class userProfileSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+
+    def validate(self, attrs):
+        username = str(self.context.get('request').data.get('username'))
+        password = str(self.context.get('request').data.get('password'))
+        email = str(self.context.get('request').data.get('email'))
+        if len(username.strip()) < 3:
+            raise serializers.ValidationError("Username too short")
+        else:
+            if len(User.objects.filter(username=username)) != 0:
+                raise serializers.ValidationError("Username exists")
+        if len(password.strip()) < 7:
+            raise serializers.ValidationError("Password too short")
+        if not validate_email(email):
+            raise serializers.ValidationError("Bad email")
+        return {'attrs': {'username': username, 'password': password, 'email': email}}
+
+    def create(self, validated_data):
+        attrs = validated_data.get('attrs')
+        user = User.objects.create(
+            username=attrs.get('username'),
+            email=attrs.get('password'),
+            password=make_password(attrs.get('password')))
+        user_profile = userProfile.objects.create(user=user)
+        return user_profile
+
+    class Meta:
+        model = userProfile
+        fields = '__all__'
---
+++ accounts/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView, UpdateAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from django.contrib.auth.hashers import check_password, make_password
+
+from .models import userProfile
+from .serializers import userProfileSerializer
+
+
+class CreateProfile(CreateAPIView):
+    """
+    post:
+      Создание профиля пользователя
+    """
+    serializer_class = userProfileSerializer
+    permission_classes = [AllowAny]
+
+    def get_serializer_class(self):
+        return self.serializer_class
+
+
+class GetProfile(APIView):
+    """
+    get:
+        Получение профиля пользователя
+    """
+    serializer_class = userProfileSerializer
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        username = self.request.user
+        user = User.objects.get(username=username)
+        print(user)
+        user_profile = userProfile.objects.filter(user=user)
+        print(user_profile)
+        user_info = {
+            'username': user.username,
+            'email': user.email,
+            'forms': ['formName']
+        }
+        return Response(user_info)
+
+    def post(self, request):
+        username = request.POST['login']
+        password = request.POST['password']
+        try:
+            usr_obj = User.objects.get(username=username)
+            if check_password(password, usr_obj.password):
+                return Response({'status': 'ok'})
+            else:
+                return Response({'status': 'bad_pass'})
+        except:
+            return Response({'status': 'error'})
+
+
+class UpdateProfile(UpdateAPIView):
+    """
+        patch:
+            Изменение данных профиля пользователя
+        """
+    serializer_class = userProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def patch(self, request, *args, **kwargs):
+        pass
