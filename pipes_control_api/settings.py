98b5a1b4003ff74c3d9aaf16aac6ca1b96f99044
NickPYTH <nicksrnk@gmail.com>
2021-11-28 12:09:33 +0500

    changed responce

--- pipes_control_api/settings.py
+++ pipes_control_api/settings.py
@@ -83,7 +83,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
     }
+}
+DATABASE_OPTIONS = {
+    "charset": "utf8",
 }
 
 
